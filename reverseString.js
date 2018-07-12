393c218cd73f1f8f67612b75ed2a38a9b7bf79a3
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-12 09:46:59 -0500

    Set up capitalizeLetter.js

--- reverseString.js
+++ reverseString.js
@@ -1,3 +1,6 @@
+// Write a function that takes a string as input and returns the string reversed
+// i.e. “Hello” will be returned as “olleH”
+
 function reverseString(string) {
     let splitString = string.split('');
     let reversedSplitString = splitString.reverse();
