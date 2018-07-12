1488b2372dfe4fe72719da9d692fe31aea828210
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-12 15:16:29 -0500

    resolved palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -4,3 +4,23 @@
 // // Write a function that takes a user input and checks to s
 // ee if it is a Palindrome and reports the result
 
+function isPalindrome(string){
+    let wordSpelledForward = string;
+    let wordSpelledBackward = wordSpelledForward.split('').reverse().join('');
+    if (wordSpelledForward === wordSpelledBackward) {
+        return message() + ' true';
+    }
+    else {
+        return message() + ' false';
+    }
+}
+
+function message() {
+    return 'The word is a Palindrome:';
+}
+
+let result1 = isPalindrome('racecar');
+console.log(result1);
+
+let result2 = isPalindrome('Trent');
+console.log(result2);
