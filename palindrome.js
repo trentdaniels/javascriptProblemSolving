cce9611036771a5065eb277b2f6c31ee5d896036
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-12 15:06:20 -0500

    Began palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,6 @@
+// // Palindrome
+// // A word, phrase, or sequence that reads the 
+// same backward as forward i.e. madam
+// // Write a function that takes a user input and checks to s
+// ee if it is a Palindrome and reports the result
+
