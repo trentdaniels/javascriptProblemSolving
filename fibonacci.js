f386e6fbd50af6a882b4f996f7cb01ee4cf252ed
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-17 10:11:59 -0500

    resolved fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -7,3 +7,34 @@
 
 // HARDER VERSION: Write a function that does the Fibonacci sequence 
 // starting at a number that a user inputs
+
+function writeFibonacciSequence() {
+    let numberOfDigits = parseInt(prompt('How many spots would you like the sequence to be?'));
+    if (numberOfDigits > 0) {
+        let total = 0;
+        let resultArray = [1];
+        for (let i = 1; i < numberOfDigits; i++) {
+            if (i === 1) {
+                total = 1;
+            }
+            else {
+                total = resultArray[i-2] + resultArray[i-1];
+            }
+            
+            resultArray.push(total);
+        }
+        return resultArray.join(', ');
+    }
+    else {
+        return 'Oops! Please refresh and enter a valid number';
+    }
+    
+}
+
+let fibonacciSequence = writeFibonacciSequence();
+console.log(fibonacciSequence);
+
+//get numbers in the sequence
+//start with 1
+//add number to it's previous number and add total to string
+//print result
