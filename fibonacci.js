cf043869a7ee052ece1555cbae88663c92e976d2
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-17 08:48:26 -0500

    created fibonacci.js with prompt

---
+++ fibonacci.js
@@ -0,0 +1,9 @@
+// Fibonacci
+// A series of numbers in which each number (Fibonacci number ) 
+// is the sum of the two preceding numbers. 
+
+// The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+// Write a function that does the Fibonacci sequence starting at 1
+
+// HARDER VERSION: Write a function that does the Fibonacci sequence 
+// starting at a number that a user inputs
