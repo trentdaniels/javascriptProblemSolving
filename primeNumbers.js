90aa1615a36a12ff39fe7d197b886acbfd1c0b7d
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-17 08:46:13 -0500

    resolved primeNumbers.js

--- primeNumbers.js
+++ primeNumbers.js
@@ -3,32 +3,24 @@
 // Write a function that prints out all prime numbers between 1 and 100
 
 
-
 //Loop through 1 and 100
 //Check if a prime number
 //Console.log only prime Numbers
 
-function isPrimeNumber(number) {
-    for (let i = 1; i <= number; i++) {
-        for (let j = 1; j <= number; j++) {
-            if (number / j === number && number / j === 1) {
-                return true;
+
+function printPrimeNumbers() {
+    for (let i = 1; i <=100; i ++) {
+        let isPrime = true;
+        for (let j = 2; j <= i; j++) {
+            if (i % j === 0 && i !== j) {
+                isPrime = false;
             }
-            else {
-                return false;
-            }
+        }
+        if (isPrime) {
+            console.log(i);
         }
     }
 }
 
-function printPrimeNumber() {
-    let arrayOfNumbers = [];
-    for(let i = 1; i <=100; i++) {
-        if(isPrimeNumber(i)) {
-            arrayOfNumbers.push(i);
-        }
-    }
-    console.log(arrayOfNumbers.split(', '));
-}
+printPrimeNumbers();
 
-printPrimeNumber();
